e2a507b08f2b8e4dbcef727c61e4c3bc31836c90
mtracy01 <matthetracy@gmail.com>
2015-05-27 14:31:37 -0400

    Added some organizational elements

---
+++ cic-android/mobile/src/main/java/modular/cic/MainComponents/DeviceListAdapter.java
@@ -0,0 +1,8 @@
+package modular.cic.MainComponents;
+
+/**
+ * Created by matthew on 5/27/15.
+ * This will be the list adapter used for displaying devices in the main activity.
+ */
+public class DeviceListAdapter {
+}

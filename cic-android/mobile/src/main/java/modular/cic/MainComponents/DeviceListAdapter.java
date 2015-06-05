9f2c1cbc5f6c5c7d82dc0dbf06426a247f845f85
mtracy01 <matthetracy@gmail.com>
2015-06-05 17:23:20 -0400

    Finished implementation of DeviceListAdapter

--- cic-android/mobile/src/main/java/modular/cic/MainComponents/DeviceListAdapter.java
+++ cic-android/mobile/src/main/java/modular/cic/MainComponents/DeviceListAdapter.java
@@ -1,10 +1,16 @@
 package modular.cic.MainComponents;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.media.Image;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import modular.cic.R;
 
@@ -14,12 +20,14 @@ import modular.cic.R;
  * This will be the list adapter used for displaying devices in the main activity.
  */
 public class DeviceListAdapter extends ArrayAdapter<String>{
-    private Image[] image;      //Image for device
+    private Activity context;
+    private Bitmap[] image;      //Image for device
     private String[] name;      //Name of device
     private int[] status;       //Status of device
 
-    public DeviceListAdapter(Activity context, String[] names, Image[] images, int[] statuss){
+    public DeviceListAdapter(Activity context, String[] names, Bitmap[] images, int[] statuss){
         super(context,R.layout.devicelistadapter,names);
+        this.context=context;
         this.image = images;
         this.name = names;
         this.status = statuss;
@@ -27,7 +35,35 @@ public class DeviceListAdapter extends ArrayAdapter<String>{
 
     @Override
     public View getView(int position, View view, ViewGroup parent){
-        //TODO: Finish this
-        return null;
+        LayoutInflater inflater = context.getLayoutInflater();//TODO: Finish this
+        View rowView = inflater.inflate(R.layout.devicelistadapter, null, true);
+        TextView textView = (TextView) rowView.findViewById(R.id.txt);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+        RadioButton bttn = (RadioButton) rowView.findViewById(R.id.bttn);
+
+        //Set values for row
+        textView.setText(name[position]);
+        imageView.setImageBitmap(image[position]);
+        bttn.setChecked(true);
+
+        switch(status[position]) {
+            case 0:
+                //Device offline / unavailable
+                bttn.setTextColor(Color.RED);
+                break;
+            case 1:
+                //Device sleeping
+                bttn.setTextColor(Color.YELLOW);
+                break;
+            case 2:
+                //Device online / is primary receiver
+                bttn.setTextColor(Color.GREEN);
+                break;
+            default:
+                //Status unknown
+                bttn.setTextColor(Color.BLACK);
+                break;
+        }
+        return rowView;
     }
 }

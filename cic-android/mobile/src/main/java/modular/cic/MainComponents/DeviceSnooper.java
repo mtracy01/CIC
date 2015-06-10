cb60d7cffc9dfb69acb1f44e4bf7a9aa05a63398
mtracy01 <matthetracy@gmail.com>
2015-06-10 13:38:29 -0400

    added DeviceSnooper class

---
+++ cic-android/mobile/src/main/java/modular/cic/MainComponents/DeviceSnooper.java
@@ -0,0 +1,21 @@
+package modular.cic.MainComponents;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import modular.cic.Objects.Device;
+
+/**
+ * Created by matthew on 6/10/15.
+ * Purpose: Gather device info, add it to the database if necessary
+ */
+public class DeviceSnooper {
+    //Return non-null device if we should prompt user to add this device to their devices
+    public static Device gatherDeviceInfo(Context context){
+        //Get device id
+        String hid = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+        
+        return null;
+    }
+
+}

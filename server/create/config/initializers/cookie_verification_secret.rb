5668894e6ada6e616eed70530165d21a3471f972
mtracy01 <matthetracy@gmail.com>
2015-10-23 13:54:05 -0400

    Removed server folder, currently using Parse

--- server/create/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'b9b0968c8be0bc207dc699c17d2241744eb65de794d0dfb0d32a9023ab9ea7d8ea7a83e1b833759ed164644262e30df102027e0060da14c86e4c7a142a275742';

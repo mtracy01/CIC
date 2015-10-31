9a362677916225c568b304e6c2e946afdfee547a
mtracy01 <matthetracy@gmail.com>
2015-10-31 14:25:04 -0400

    code refactoring and reduction

--- cic-android/mobile/src/main/java/modular/cic/HelperComponents/App.java
+++ cic-android/mobile/src/main/java/modular/cic/HelperComponents/App.java
@@ -9,10 +9,6 @@ import java.util.ArrayList;
  * Purpose: Basic helper class for mundane tasks (such as killing the app)
  */
 public class App {
-    public static void kill() {
-        android.os.Process.killProcess(android.os.Process.myPid());
-    }
-
     public static boolean first = false;
     public static ArrayList<ParseObject> devices = new ArrayList<>();
 }
